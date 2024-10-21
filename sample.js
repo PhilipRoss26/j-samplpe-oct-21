fbe8ed0ac8614f2d523c49ae6e49c43ad2c2112e
PhilipRoss26 <philipross2468@gmail.com>
2024-10-21 11:24:24 -0400

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,7 @@
+const setUp = ()=>{
+    $('#name').text("Philip Ross");
+    $('#major').text("CIT");
+    $('#year').text("Junior");
+    $('#location').html("<em>America</em>");
+}
+$(document).ready(setUp)
